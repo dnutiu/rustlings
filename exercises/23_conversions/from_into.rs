7cd24e4973c451573c4176d975a53f87a9999b8a
Denis Nutiu <denis.nutiu@gmail.com>
2024-11-09 13:43:57 +0200

    record progress: clippy, macros

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -20,7 +20,7 @@ impl Default for Person {
     }
 }
 
-// TODO: Complete this `From` implementation to be able to parse a `Person`
+// Complete this `From` implementation to be able to parse a `Person`
 // out of a string in the form of "Mark,20".
 // Note that you'll need to parse the age component into a `u8` with something
 // like `"4".parse::<u8>()`.
@@ -34,7 +34,31 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let elements: Vec<&str> = s.split(",").collect();
+
+        match elements.as_slice() {
+            [a, b] => {
+                if let Ok(age) = b.parse::<u8>() {
+                    let name = a.parse().unwrap();
+                    if name == "" {
+                        return Person {
+                            name: String::from("John"),
+                            age: age,
+                        };
+                    }
+                    return Person {
+                        name: name,
+                        age: age,
+                    };
+                };
+                Person::default()
+            }
+            &_ => {
+                Person::default()
+            }
+        }
+    }
 }
 
 fn main() {
